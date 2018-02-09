3fa3b9ebb71be87d5ae6de378c0c39eead4ded38
woravich-k <woravich@hotmail.com>
2018-02-08 19:09:59 -0500

    Replace buttons by toggle switches

--- uczlumt/www/js/appActivity.js
+++ uczlumt/www/js/appActivity.js
@@ -10,66 +10,6 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
 	id: 'mapbox.streets'
 	}).addTo(mymap);
 
-//UCL switch
-var UCL;
-var UCLbox = document.getElementById("UCLbox");	
-function UCLpoint()	{
-//add a point
-	if (UCLbox.checked){
-		UCL = L.marker([51.5,-0.09]).addTo(mymap).bindPopup("<b>Hello world!</b><br/>I am a popup.<br/><a href='http://www.ucl.ac.uk'>Go to UCL website</a>").openPopup();
-		mymap.flyToBounds(UCL.getLatLng().toBounds(500));
-		autoPan = false;
-	} else {
-		mymap.removeLayer(UCL);
-	}
-}
-
-//Earthquakes switch
-var EQbox = document.getElementById("EQbox");	
-function EQ()	{
-//add a point
-	if (EQbox.checked){
-		getData('earthquakes');
-	} else {
-		removeData('earthquakes');
-	}
-}
-
-//Bus switch
-var Busbox = document.getElementById("Busbox");	
-function bus()	{
-//add a point
-	if (Busbox.checked){
-		getData('busstops');
-	} else {
-		removeData('busstops');
-	}
-}
-
-	
-// //add a circle
-	
-function addCircle(){
-	L.circle([51.508,-0.11],500,{
-		color: 'red',
-		fillColor: '#f03',
-		fillOpacity: 0.5
-	}).addTo(mymap).bindPopup("I am a circle.");
-}
-		
-//add a polygon with 3 end points (i.e. a triangle)
-function addPolygon(){
-	var myPolygon = L.polygon([
-		[51.509,-0.08], 
-		[51.503,-0.06], 
-		[51.51,-0.047]
-	],{
-		color: 'blue',
-		fillColor: '#096aea',
-		fillOpacity: 0.2
-	}).addTo(mymap).bindPopup("I am a polygon.");
-}
-
 // //create a variable that will hold the XMLHttpRequest() - this must be done outside a function so that all the functions  can use the sam variable
 // var client;
 		
@@ -86,57 +26,6 @@ var testMarkerPink = L.AwesomeMarkers.icon({
 	markerColor: 'pink'
 });
 		
-// //create the code to get the Earthquakes data using an XMLHttpRequest
-// function getEarthquakes(){
-	// client = new XMLHttpRequest();
-	// client.open('GET','https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson');
-	// client.onreadystatechange = earthquakeResponse; //note don't use earthquakeResponse() with brackets as that doesn't work
-	// client.send();
-// }
-		
-// //create the code to wait for the response from the data server, and process the response once it is received
-// function earthquakeResponse(){
-// //this function listens out for the server to say that the data is ready - i.e. has state 4
-	// if (client.readyState == 4){
-	// //once the data is ready, process the data
-	// var earthquakedata = client.responseText;
-	// loadEarthquakelayer(earthquakedata);
-	// }
-// }
-		
-// //convert the received data - which is text - to JSON format and add it to the map
-// function loadEarthquakelayer(earthquakedata){
-	// //convert the text to JSON
-	// var earthquakejson = JSON.parse(earthquakedata);
-	
-	// //add the JSON layer onto the map it will appear using the default icons
-	// earthquakelayer = L.geoJson(earthquakejson,
-	// {
-		// //use point to layer to create the points
-		// pointToLayer: function(feature, latlng){
-			// //look at the GeoJSON filr - specifically at the properties - to see the earthquake magnitude and use a different marker depending on this value
-			// //also include a pop-up that shows the place value of the earthquakes 
-			// if (feature.properties.mag>1.75){
-				// return L.marker(latlng,{icon:testMarkerRed}).bindPopup("<b> location: "+feature.properties.place+"</b>");
-			// }else{
-			// //mag is lower than 1.75
-				// return L.marker(latlng,{icon:testMarkerPink}).bindPopup("<b> location: "+feature.properties.place+"</b>");
-			// }
-		// }
-	// }).addTo(mymap);
-	
-	// //change the map zoom so that all the data is shown
-	// mymap.fitBounds(earthquakelayer.getBounds());
-	// autoPan = false;
-	
-// }
-
-// // //load the map data (earthquake) after the page has loaded	
-// // document.addEventListener('DOMContentLoaded',function(){
-	// // getEarthquakes();
-// // },false);
-
-
 //create a variable that will hold the XMLHttpRequest() - this must be done outside a function so that all the functions  can use the sam variable
 var client;
 
@@ -159,7 +48,7 @@ function getData(layername){
 		alert("The layer is not loaded to the map, since it has already been existed.")
 		return
 	}
-	alert("Loading")
+	//alert("Loading")
 	client = new XMLHttpRequest();
 	
 	client.open('GET', url);
@@ -217,7 +106,7 @@ function removeData(layername){
 //check whether the layer is existed on the map or not if not inform the user.
 	if (layername == "earthquakes") {
 		if (loadingEarthquakes){
-			alert("removing the earthquake data here");
+			//alert("removing the earthquake data here");
 			mymap.removeLayer(earthquakelayer);
 			loadingEarthquakes = false;
 		} else {
@@ -226,7 +115,7 @@ function removeData(layername){
 	}
 	if (layername == "busstops") {
 		if (loadingBusstops){
-			alert("removing the busstops data here");
+			//alert("removing the busstops data here");
 			mymap.removeLayer(busstoplayer);
 			loadingBusstops = false;
 		} else {
@@ -241,6 +130,7 @@ function removeData(layername){
 
 //Tracking location
 var currentLocationLyr;
+var id;
 var firstTime = true;
 var previousBound;
 var autoPan = false;
@@ -248,15 +138,14 @@ var autoPan = false;
 function trackLocation() {
 	if (!firstTime){
 	// zoom to center
-		mymap.flyToBounds(currentLocationLyr.getLatLng().toBounds(250));
-		mymap.on('zoomend', function() {
-			autoPan = true;
-		});
+		mymap.fitBounds(currentLocationLyr.getLatLng().toBounds(250));
+		autoPan = true;
+		
 		
 	} else {
 		if (navigator.geolocation) {
 			alert("Getting current location");
-			navigator.geolocation.watchPosition(showPosition);
+			id = navigator.geolocation.watchPosition(showPosition);
 			
 			
 		} else {
@@ -274,7 +163,7 @@ function showPosition(position) {
 	
 	if(firstTime){
 		firstTime = false;
-		mymap.flyToBounds(currentLocationLyr.getLatLng().toBounds(250));
+		mymap.fitBounds(currentLocationLyr.getLatLng().toBounds(250));
 		autoPan = true;
 	}else if (autoPan) {
 		mymap.panTo(currentLocationLyr.getLatLng());
@@ -286,7 +175,112 @@ function showPosition(position) {
 mymap.on('dragstart', function() {
 	autoPan = false;
 })
+
+
 function alertAuto(){
 	alert(autoPan);
 	
 }
+
+
+
+
+
+//Earthquakes switch
+var EQbox = document.getElementById("EQbox");	
+function EQ()	{
+//add a point
+	if (EQbox.checked){
+		getData('earthquakes');
+	} else {
+		removeData('earthquakes');
+	}
+}
+
+//Bus switch
+var Busbox = document.getElementById("Busbox");	
+function bus()	{
+//add a point
+	if (Busbox.checked){
+		getData('busstops');
+	} else {
+		removeData('busstops');
+	}
+}
+
+//Current location switch
+var locationBox = document.getElementById("locationBox");	
+function currentLocation()	{
+//add a point
+	if (locationBox.checked){
+		trackLocation();
+	} else {
+		navigator.geolocation.clearWatch(id);
+		mymap.removeLayer(currentLocationLyr);
+		firstTime = true;
+		autoPan = false;
+		
+	}
+}
+
+
+//UCL switch
+var UCL;
+var UCLbox = document.getElementById("UCLbox");	
+function UCLpoint()	{
+//add a point
+	if (UCLbox.checked){
+		autoPan = false;
+		UCL = L.marker([51.5,-0.09]).addTo(mymap).bindPopup("<b>Hello world!</b><br/>I am a popup.<br/><a href='http://www.ucl.ac.uk'>Go to UCL website</a>").openPopup();
+		mymap.flyToBounds(UCL.getLatLng().toBounds(500));
+	} else {
+		mymap.removeLayer(UCL);
+	}
+}
+	
+//add a circle
+var circle;
+var circle_box = document.getElementById("circle_box");		
+function addCircle(){
+	if (circle_box.checked){
+		autoPan = false;
+		circle = L.circle([51.508,-0.11],500,{
+			color: 'red',
+			fillColor: '#f03',
+			fillOpacity: 0.5
+		}).addTo(mymap).bindPopup("I am a circle.");
+		mymap.flyToBounds(circle);
+	} else {
+		mymap.removeLayer(circle);
+	}
+	
+}
+		
+//add a polygon with 3 end points (i.e. a triangle)
+var myPolygon;
+var poly_box = document.getElementById("poly_box");	
+function addPolygon(){
+	if (poly_box.checked){
+		autoPan = false;
+		myPolygon = L.polygon([
+			[51.509,-0.08], 
+			[51.503,-0.06], 
+			[51.51,-0.047]
+		],{
+			color: 'blue',
+			fillColor: '#096aea',
+			fillOpacity: 0.2
+		}).addTo(mymap).bindPopup("I am a polygon.");
+		mymap.flyToBounds(myPolygon);
+		
+	} else {
+		mymap.removeLayer(myPolygon);
+	}
+}
+
+function panToCurrentLoc(){
+	if (!firstTime) {
+		trackLocation();
+	}
+}
+
